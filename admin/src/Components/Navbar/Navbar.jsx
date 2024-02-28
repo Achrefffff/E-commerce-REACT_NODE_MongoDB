70d313ce5c8776e3c0e0946d60a1569630159813
dorra chaffai <achref.chouikh@gmail.com>
2024-02-28 11:16:49 +0100

    ajout des articles terminé

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Navbar.css'
+import navlogo from '../../assets/nav-logo.svg'
+import navProfile from '../../assets/nav-profile.svg'
+
+
 const Navbar = () => {
   return (
     <div className='navbar'>
-
+        <img src={navlogo} alt=""  className='nav-logo'/>
+        <img src={navProfile} alt="" />
     </div>
   )
 }
