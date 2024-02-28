ca9042b769124918c2f5175a310455cff52decd8
dorra chaffai <achref.chouikh@gmail.com>
2024-02-28 08:29:53 +0100

    partie front avec responsive+connexion bd

---
+++ backend/index.js
@@ -0,0 +1,131 @@
+const port = 4000;
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const multer = require('multer');
+const path = require('path');
+const cors = require('cors');
+const e = require('express');
+
+app.use(express.json());
+app.use(cors());
+
+// Connexion de la bd
+mongoose.connect('mongodb+srv://achrefchouikh:Pleasego5@cluster0.wpp4xde.mongodb.net/e-commerce');
+
+// Création de l'API
+app.get('/', (req, res) => {
+    res.send('express est en marche');
+});
+
+// Upload des images
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+// Création endpoint pour les images
+app.use('/images', express.static('upload/images'));
+
+// Utilisation du middleware upload après sa définition
+app.post('/upload', upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    });
+});
+
+
+//Shema pour creer  les produits
+const  Product=mongoose.model('Product',{
+    id:{
+        type:Number,
+        required:true
+    },
+    name:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+        required:true
+    },
+    category:{
+        type:String,
+        required:true
+    },
+    new_price:{
+        type:Number,
+        required:true
+    },
+    old_price:{
+        type:Number,
+        required:true
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    },
+    available:{
+        type:Boolean,
+        default:true
+    },
+})
+app.post('/addproduct', async(req, res) => {
+    let products = await Product.find({})
+    let id
+    if(products.length > 0) {
+        let last_product_array = products.slice(-1)  
+        let last_product = last_product_array[0]
+        id = last_product.id + 1
+    } else {
+        id = 1
+    }
+        
+    const product= new Product({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+          
+    })
+    console.log(product);
+    await product.save();
+    console.log('Enregistré');
+    res.json({
+        success:true,
+        name:req.body.name,
+    });
+});
+
+// DELETE PRODUCT
+app.post('/removeproduct',async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id})
+    console.log('supprimé');
+    res.json({
+        success:true,
+        name:req.body.name,
+    }); 
+})
+
+// API GET ALL PRODUCTS 
+app.get('/allproducts', async (req, res) => {
+    let products = await Product.find({})
+    console.log('tous les produits');
+    res.send(products)
+})
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log('server running sur le port ' + port);
+    } else {
+        console.log('server erreur : ' + error);
+    }
+});
