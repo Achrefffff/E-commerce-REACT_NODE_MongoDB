8d65ce373a48636ca9a58cec9ea40b5c953c39fb
dorra chaffai <achref.chouikh@gmail.com>
2024-02-29 10:35:02 +0100

    endpoint addToCart et removeFromCart

--- backend/index.js
+++ backend/index.js
@@ -122,6 +122,131 @@ app.get('/allproducts', async (req, res) => {
     res.send(products)
 })
 
+// le schéma pour creer les utilisateurs
+const Users = mongoose.model('Users', {
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    password: {
+        type: String,
+    },
+    cartData: {
+        type: Object,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+});
+
+// API pour enregistrer un utilisateur
+app.post('/signup', async (req, res) => {
+    let check = await Users.findOne({ email : req.body.email });
+    if(check) {
+        return res.status(400).json({success: false, errors: 'Email already exists'});
+    }
+    let cart={}
+    for (let i = 0; i < 300; i++) {
+       cart[i]=0       
+    }
+    const user = new Users({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart,
+    });
+
+    await user.save();
+
+    const data= {
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success: true, token});
+});
+
+// API pour se connecter
+app.post('/login', async (req, res) => {
+    let user = await Users.findOne({email: req.body.email});
+    if (user) {
+        const passCompare = req.body.password === user.password;
+        if(passCompare) {
+            const data= {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data,'secret_ecom');
+            res.json({success: true, token});
+        }
+        else {
+            res.json({success: false, errors: 'Invalid password'});
+        }
+
+    } else {
+        res.json({success: false, errors: 'Invalid email'});
+    }
+})
+
+// nouvelle collection data
+app.get('/newcollection', async (req, res) => {
+    let products = await Product.find({})
+    let newCollection = products.slice(1).slice(-8)
+    console.log('nouvelle collection fetched' );
+    res.send(newCollection)
+})
+
+//end point pour popular women section
+app.get('/popularinwomen', async (req, res) => { 
+    let products = await Product.find({category:'women'})
+    let popular_in_women = products.slice(0, 4)
+    console.log('popular in women fetched')
+    res.send(popular_in_women)
+})
+// middleware pour fetch  le user
+
+const fetchUser = async(req, res, next) => {
+    const token= req.header('auth-token');
+    if (!token) {
+         res.status(401).send({errors:'Acces denied'});
+    } else {
+        try {
+            const data=jwt.verify(token,'secret_ecom');
+            req.user=data.user;
+            next(); 
+    } catch (error) {
+        res.status(401).send({errors:'Invalid token'});
+    }
+}
+}
+
+// ajout de produit au panier
+
+app.post('/addtocart',fetchUser, async (req, res) => {
+    console.log('added',req.body.itemId);
+   let userData = await Users.findOne({_id:req.user.id})
+   userData.cartData[req.body.itemId] += 1
+   await Users.findOneAndUpdate({_id:req.user.id}, {cartData: userData.cartData})
+   res.send('added')
+})
+
+// remove product from cart
+app.post('/removefromcart',fetchUser, async (req, res) => {
+    console.log('removed',req.body.itemId);
+    let userData = await Users.findOne({_id:req.user.id})
+    if( userData.cartData[req.body.itemId]>0)
+    userData.cartData[req.body.itemId] -= 1
+    await Users.findOneAndUpdate({_id:req.user.id}, {cartData: userData.cartData})
+    res.send('Removed')
+})
 app.listen(port, (error) => {
     if (!error) {
         console.log('server running sur le port ' + port);
